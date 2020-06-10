3782246abb2b46f96cb59443bed10504e5278cb5
Domenico Delvalle <domedfd@gmail.com>
2020-06-10 03:48:33 -0400

    List all tasks

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -25,6 +25,17 @@ class TaskController {
         return res.status(500).json(error);
       });
   }
+  //FUNCION LISTAR TODAS TAREAS
+  async all(req, res) {
+    await TaskModel.find()
+      .sort("created")
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
+      });
+  }
 }
 
 module.exports = new TaskController();
