7178dd96ef08f547c7485c27de3aab07bf30bdfa
Domenico Delvalle <domedfd@gmail.com>
2020-06-10 12:28:34 -0400

    Install cors

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const bot = require("./controller/bot");
 bot.launch();
 
 const express = require("express");
+const cors = require("cors");
 
 const server = express();
+server.cors(cors());
 server.use(express.json());
 
 const TaskRoutes = require("./routes/TaskRoutes");
