bbeeaa10e5d9659f9403669d121f44b8b4626ae2
Domenico Delvalle <domedfd@gmail.com>
2020-06-13 05:13:37 -0400

    nodemon

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 const url = "mongodb://domenico:apostala123@ds145997.mlab.com:45997/heroku_vq5939k4";
-mongoose.connect(url, { useNewUrlParser: true });
+mongoose.connect(url, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 
 module.exports = mongoose;
