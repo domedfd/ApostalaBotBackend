4bfa40cf46d936dfc70fecf3c49b0de099b8736e
Domenico Delvalle <domedfd@gmail.com>
2020-06-13 05:35:44 -0400

    port

--- src/index.js
+++ src/index.js
@@ -16,5 +16,5 @@ var server_port = process.env.YOUR_PORT || process.env.PORT || 3333;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 server.listen(server_port, () => {
-  console.log("API ONLINE");
+  console.log("API ONLINE "+ server_port);
 });
